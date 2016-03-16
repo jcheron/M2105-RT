ca42e719d1500b32c4d4857483a6f1f43748f4ac
JoretGaetan <21500843@etu.unicaen.fr>
2016-03-16 15:16:43 +0100

    EX 1
    EXERCICE 1

---
+++ tp2/ex1verification.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+<p>Bonjour !</p>
+<p>Je sais comment tu t'appelles, hé hé. Tu t'appelles <?php echo $_POST['nom']; ?> !</p>
+<p>Si tu veux changer de prénom, <a href="ex1.php">clique ici</a> pour revenir à la page d'acceuil</p>
+       </body>
+</html> 
