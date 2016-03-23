92545b143ad477337650143d4046155d55dc5a65
jcheron <myaddressmail@gmail.com>
2016-03-23 15:59:01 +0100

    after last

---
+++ tp2/ex7/ex7.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: heronj
+ * Date: 23/03/2016
+ * Time: 15:28
+ */
+include '../functions.php';
+getHeader(true, "Exercice 7");
+afficheQuestions(getQuestions());
