cea3d1a7eea818ecc5de74e3598437f94ba1e518
jcheron <myaddressmail@gmail.com>
2016-03-24 17:05:19 +0100

    Update allSessionVars.php

--- tp2/allSessionVars.php
+++ tp2/allSessionVars.php
@@ -1,3 +1,11 @@
+<?php
+include "functions.php";
+getHeader(true,"");
+if(array_key_exists("delete",$_GET)){
+    resetSession();
+};
+?>
+<div class="jumbotron">
 <h1>Liste des variables de session</h1>
 <?php
 /**
@@ -6,7 +14,19 @@
  * Date: 16/03/2016
  * Time: 15:57
  */
-session_start();
-foreach($_SESSION as $k=>$v){
-    echo $k."=>".$v."<br>";
-}
+?>
+<div class="list-group">
+    <ul>
+        <?php
+        foreach ($_SESSION as $k=>$v){
+            if(is_array($v)){
+                echo "<li class='list-group-item'>{$k} =>".implode(' ',$v)."</li>";
+            }else {
+                echo "<li class='list-group-item'>{$k} => {$v}</li>";
+            }
+        }
+        ?>
+    </ul>
+</div>
+<a class="btn btn-primary" href="<?php echo($_SERVER["PHP_SELF"]."?delete")?>">Supprimer les variables de session</a>
+</div>
